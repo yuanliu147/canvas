5f3fd10352cc2f8d987ff726c7cfe11828c505d5
liuyuan01 <liuyuan@aisino.com>
2021-11-30 17:53:08 +0800

    add: 时钟代码封装-1

--- 4.时钟/useClock.js
+++ 4.时钟/useClock.js
@@ -1,12 +1,25 @@
-function draw(ctx, callBack) {
+// 绘制包裹
+function draw(ctx, callBack, ...args) {
     ctx.save()
-    callBack()
+    callBack(ctx, ...args)
     ctx.restore()
 }
-
+// 绘制圆盘
 function drawDisk(ctx) {
     ctx.beginPath()
     ctx.fillStyle = 'black'
     ctx.arc(clockX, clockY, 66, 0, Math.PI * 2)
     ctx.fill()
+}
+// 绘制圆盘边框
+function drawDiskFrame(ctx) {
+    ctx.beginPath()
+    ctx.lineWidth = 8
+    ctx.strokeStyle = '#f15252'
+    ctx.arc(clockX, clockY, 70, 0, Math.PI * 2)
+    ctx.stroke()
+}
+// 绘制圆盘边框描边
+function drawDiskFrameStroke(ctx) {
+
 }
